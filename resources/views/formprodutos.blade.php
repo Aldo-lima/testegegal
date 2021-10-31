fa63652406692c4a4fe2e583b98d787190db78cd
Aldo Cesar <57098697+Aldo-lima@users.noreply.github.com>
2021-10-31 11:16:31 -0300

    hvvgvhg

--- resources/views/formprodutos.blade.php
+++ resources/views/formprodutos.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 @section('container-principal')
 <form action="{{route('produto.store')}}" method="POST" >
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    @csrf
     <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">Nome</label>
       <input type="text" name="nome"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -12,7 +12,6 @@
         <input type="text" name="descricao" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
-
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
 @endsection
